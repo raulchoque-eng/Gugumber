5c6fde63399211858da4f201fa91780a5285066c
Areliez Vargas <areliez.vargas@gmail.com>
2019-05-24 22:00:33 -0400

    Add navigate to home

--- src/test/java/org/fjala/gugumber/steps/LoginSteps.java
+++ src/test/java/org/fjala/gugumber/steps/LoginSteps.java
@@ -13,9 +13,11 @@
 
 package org.fjala.gugumber.steps;
 
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import org.fjala.gugumber.salesforce.ui.LoginPage;
 import org.fjala.gugumber.salesforce.ui.PageTransporter;
+import org.fjala.gugumber.salesforce.ui.pages.HomePage;
 
 /**
  * LoginSteps class.
@@ -24,12 +26,14 @@ import org.fjala.gugumber.salesforce.ui.PageTransporter;
  * @version 0.0.1
  */
 public class LoginSteps {
-    PageTransporter pageTransporter = PageTransporter.getInstance();
-    LoginPage loginPage;
+    private PageTransporter pageTransporter = PageTransporter.getInstance();
+    private LoginPage loginPage;
+    private HomePage homePage;
 
-    @When("^I am logged in SalesForce with \"([^\"]*)\" and \"([^\"]*)\"$")
+    @Given("^I am logged in SalesForce with \"([^\"]*)\" and \"([^\"]*)\"$")
     public void logInWithUsernameAdminAndPasswordAdmin(String username, String password) {
         loginPage = pageTransporter.navigateToLoginPage();
         loginPage.login(username, password);
+        homePage = pageTransporter.navigateToHomePage();
     }
 }
