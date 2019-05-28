411d3b6dfe9e0488e448bdacf91f3dcbca7b5c5e
Areliez Vargas <areliez.vargas@gmail.com>
2019-05-27 23:40:44 -0400

    Implements the strategy to set the account information

--- src/main/java/org/fjala/gugumber/salesforce/entities/Account.java
+++ src/main/java/org/fjala/gugumber/salesforce/entities/Account.java
@@ -12,11 +12,66 @@
 
 package org.fjala.gugumber.salesforce.entities;
 
+import org.fjala.gugumber.salesforce.utils.StrategySetter;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Account class.
  *
- * @author Raul Choque
+ * @author Areliez Vargas
  * @version 0.0.1
  */
 public class Account {
+
+    /**
+     * Constant for key of name account.
+     */
+    private static final String NAME_ACCOUNT = "Name Account";
+
+    /**
+     * Variable for the name account of an Account.
+     */
+    private String nameAccount;
+
+    /**
+     * Returns the name account.
+     *
+     * @return name account as string.
+     */
+    public String getNameAccount() {
+        return nameAccount;
+    }
+
+    /**
+     * Sets the name of an Account sending a string.
+     *
+     * @param nameAccount for the name of the account.
+     */
+    public void setNameAccount(String nameAccount) {
+        this.nameAccount = nameAccount;
+    }
+
+    /**
+     * Sets the values of an Account sending a map with the information to set.
+     *
+     * @param accountMap to set the information.
+     */
+    public void setAccountInformation(Map<String, String> accountMap) {
+        HashMap<String, StrategySetter> strategyMap = composeStrategyMap(accountMap);
+        accountMap.keySet().forEach(key -> strategyMap.get(key).executeMethod());
+    }
+
+    /**
+     * Returns a map with the information the account.
+     *
+     * @param accountMap to set the information.
+     * @return strateg ma with the information of account.
+     */
+    private HashMap<String, StrategySetter> composeStrategyMap(Map<String, String> accountMap) {
+        HashMap<String, StrategySetter> strategyMap = new HashMap<>();
+        strategyMap.put(NAME_ACCOUNT, () -> setNameAccount(accountMap.get(NAME_ACCOUNT)));
+        return strategyMap;
+    }
 }
