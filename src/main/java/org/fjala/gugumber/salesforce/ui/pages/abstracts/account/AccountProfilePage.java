d7f93760ce004dda94f10b8221be70f7af2a5cce
Areliez Vargas <areliez.vargas@gmail.com>
2019-05-27 23:29:02 -0400

    Add the classes abstract for account

---
+++ src/main/java/org/fjala/gugumber/salesforce/ui/pages/abstracts/account/AccountProfilePage.java
@@ -0,0 +1,24 @@
+/*
+ * @(#) AccountProfilePage.java Copyright (c) 2019 Jala Foundation.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Jala
+ * Foundation, Inc. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ */
+
+package org.fjala.gugumber.salesforce.ui.pages.abstracts.account;
+
+import org.fjala.gugumber.salesforce.ui.pages.abstracts.BasePage;
+
+/**
+ * AccountProfilePage abstract class.
+ *
+ * @author Areliez Vargas
+ * @version 0.0.1
+ */
+public abstract class AccountProfilePage extends BasePage {
+}
