97b0e05e1aef8b0e2576545c4649510fe5b4c873
raulchoque <yfert_0209@hotmail.com>
2019-06-05 03:44:06 -0400

    Fix url package in EventDetail class

--- src/main/java/org/fjala/gugumber/salesforce/ui/pages/event/EventDetail.java
+++ src/main/java/org/fjala/gugumber/salesforce/ui/pages/event/EventDetail.java
@@ -10,7 +10,7 @@
  * with Jala Foundation.
  */
 
-package org.fjala.gugumber.salesforce.ui.pages.events;
+package org.fjala.gugumber.salesforce.ui.pages.event;
 
 import java.util.HashMap;
 import java.util.Map;
