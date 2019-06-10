b62d24917ad9cb8912af6604e9ae14e1a389a07d
Cristian Lujan <cxristian.lujan05@gmail.com>
2019-06-10 19:56:13 -0400

    update

--- src/main/java/org/fjala/gugumber/salesforce/ui/pages/contact/ContactDetailsAbstract.java
+++ src/main/java/org/fjala/gugumber/salesforce/ui/pages/contact/ContactDetailsAbstract.java
@@ -42,4 +42,41 @@ public abstract class ContactDetailsAbstract extends BasePage {
      * @return an Contact profile page.
      */
     public abstract ContactProfilePageAbstract openEditContactDetails();
+//    Map<String, String> contactDetailInMap = new HashMap<>();
+//
+//    /**
+//     * Gets all values from event detail.
+//     *
+//     * @param keyContact is the keys of Event into a set.
+//     */
+//    public Map<String, String> getContactDetail(final Set<String> keyContact) {
+//        final HashMap<String, StrategySetter> strategyMap = composeStrategyMap();
+//        keyContact.forEach(key -> strategyMap.get(key).executeMethod()
+//
+//        );
+//        return contactDetailInMap;
+//    }
+//
+//    /**
+//     * Composes an Strategy Map.
+//     *
+//     * @return an instance HashMap with keys and methods to run.
+//     */
+//    public HashMap<String, StrategySetter> composeStrategyMap() {
+//        final HashMap<String, StrategySetter> contactDetail = new HashMap<>();
+//        contactDetail.put(NAME, () -> contactDetailInMap.put(NAME, getNameContact()));
+//        contactDetail.put(ACCOUNT, () -> contactDetailInMap.put(ACCOUNT, getAccount()));
+//        contactDetail.put(PHONE, () -> contactDetailInMap.put(PHONE, getPhone()));
+//        contactDetail.put(EMAIL, () -> contactDetailInMap.put(EMAIL, getEmail()));
+//        contactDetail.put(HOME_PHONE, () -> contactDetailInMap.put(HOME_PHONE, getHomePhone()));
+//        contactDetail.put(TITLE, () -> contactDetailInMap.put(SALUTATION, getAssignedToUser()));
+//        contactDetail.put(DEPARTMENT, () -> contactDetailInMap.put(SALUTATION, getAssignedToUser()));
+//        contactDetail.put(MOBILE, () -> contactDetailInMap.put(SALUTATION, getAssignedToUser()));
+//        contactDetail.put(DESCRIPTION, () -> contactDetailInMap.put(DESCRIPTION, getDescription()));
+//        return contactDetail;
+//    }
+//
+//    public abstract String getAccount();
+//    public abstract String getPhone();
+//    public abstract String getEmail();
 }
